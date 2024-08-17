63fbb6639ae00ee736ec225cbd22e723d0c63909
Luiggi Antonni <luiggiantonni0129@gmail.com>
2024-08-17 08:26:03 -0300

    User Schema Created

---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } //time created and last update time
+);
+
+// Hashing password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next(); //if password is not modified
+
+  const salt = await bcrypt.genSalt(10);
+  this.password - (await bcrypt.hash(this.password, salt));
+  next();
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+};
+
+const User = mongoose.model("User", userSchema); //create model as schema
+
+module.exports = User;
+
--- server/server.js
+++ server/server.js
@@ -1,27 +1,24 @@
-const express = require('express')
-const path = require('path');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
+dotenv.config({ path: path.resolve(__dirname, "../config/.env") });
 
-const app = express()
+const app = express();
 const port = process.env.PORT;
 
 connectDB();
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 
 //In case of error
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}: http://localhost:${port}`);
-})
+  console.log(`Server running on port ${port}: http://localhost:${port}`);
+});
