b0951eddd042f2f7f2b749188e5a3d4116745ce4
Luiggi Antonni <luiggiantonni0129@gmail.com>
2024-08-17 07:45:24 -0300

    Initialize server with Express and basic configuration

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
+
+const app = express()
+const port = process.env.PORT;
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+
+//In case of error
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}: http://localhost:${port}`);
+})
