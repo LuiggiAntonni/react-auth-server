04f7f1f717ec5981528ffd1ab67aa501c42d3e64
Luiggi Antonni <luiggiantonni0129@gmail.com>
2024-08-18 10:09:03 -0300

    React implemented

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav><h1><a href="/">Luiggi AuthProject</a></h1></nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from '../logo.svg';
+
+function Home() {
+  return (    
+    <div>
+       <img src={logo} alt="logo" />
+      <h2>Welcome to Front-End Home Page!</h2>
+    </div>
+  );
+}
+
+export default Home;
